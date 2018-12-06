7c16b4e629cc8195773d0de8615ad119ad2946d6
王璐 <17612151193@163.com>
2018-12-06 10:58:09 +0800

    初始化项目
    初始化项目

---
+++ iOS界面传值的几种方式/iOS界面传值的几种方式/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS界面传值的几种方式
+//
+//  Created by 王璐 on 2018/12/6.
+//  Copyright © 2018 Ove. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS界面传值的几种方式/iOS界面传值的几种方式/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  iOS界面传值的几种方式
+//
+//  Created by 王璐 on 2018/12/6.
+//  Copyright © 2018 Ove. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
